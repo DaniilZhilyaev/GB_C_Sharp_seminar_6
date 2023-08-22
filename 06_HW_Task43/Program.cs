c37a3baa6d34cb658f2bcaf8858a5742645e8de1
daniil <daniilzhilyaev@gmail.com>
2023-08-22 20:47:46 +0300

    add HW task 43

---
+++ 06_HW_Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.WriteLine("Заданны уравнения: y = k1 * x + b1, y = k2 * x + b2");
+Console.Write("Введите члены уравнений (в последовательности b1, k1, b2 и k2): ");
+string StringArray = Console.ReadLine()!;
+string[] array = StringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+Point(array);
+
+void Point (string[] array)
+{
+    double x = 0;
+    double y = 0;
+    double[] result = new double[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = Convert.ToDouble(array[i]);
+    }
+    x = (result[2] - result[0]) / (result[1] - result[3]);
+    y = result[1] * x + result[0];
+    Console.WriteLine($"({x}; {y})");
+}
